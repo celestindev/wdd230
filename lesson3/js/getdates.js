6849f226fc5e3057f8b29efe9a86a25b8a780043
celestindev <celestingerald31@gmail.com>
2023-10-05 10:49:05 -0400

    lesson3

---
+++ lesson3/js/getdates.js
@@ -0,0 +1,5 @@
+// uses getElementById to pull last modified date and time
+document.querySelector("#lastupdated").textContent = `Last Updated: ${document.lastModified}`;
+
+// places current year into id year
+document.querySelector("#copyrightyear").textContent = new Date().getFullYear();
