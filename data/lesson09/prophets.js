472ee13f953df88525f8ef700635a1c4f9518554
celestindev <celestingerald31@gmail.com>
2023-11-08 19:17:07 -0500

    Create prophets css js JSON and index prophets

---
+++ data/lesson09/prophets.js
@@ -0,0 +1,34 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function(response){
+    return response.json();
+})
+.then(function(jsonObject){
+  const prophets = jsonObject['prophets'];
+
+  for( let i =0; i < prophets.length; i++){
+
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let birthPlace = document.createElement('p');
+    let image = document.createElement('img');
+
+    var altImg = prophets[i].name + ' ' + prophets[i].lastname;
+
+    h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+    birthDate.textContent = 'Date of Birth: '+ prophets[i].birthdate;
+    birthPlace.textContent = 'Place of birth: ' + prophets[i].birthplace;
+    image.setAttribute('src', prophets[i].imageurl);
+
+    card.appendChild(h2);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(image, image.alt = altImg);
+
+    document.querySelector('div.cards').appendChild(card);
+
+  }
+
+});
