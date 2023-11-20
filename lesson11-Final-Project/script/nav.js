d40ed6abd801c2bae7e5b9fb1963733baf34cb87
celestindev <celestingerald31@gmail.com>
2023-11-20 18:19:13 -0500

    update my form

---
+++ lesson11-Final-Project/script/nav.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+	//Animate Links
+	navLinks.classList.toggle("open");
+	links.forEach(link => {
+		link.classList.toggle("fade");
+	});
+
+	//Hamburger Animation
+	hamburger.classList.toggle("toggle");
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
+
+
+
+
+
+
